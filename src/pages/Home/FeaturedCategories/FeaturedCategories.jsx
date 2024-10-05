818d853fd490c92ebec138390b4aa0e4f0008f1d
sakib-atreus <sakibatreus@gmail.com>
2024-10-06 03:01:44 +0600

    category update

--- src/pages/Home/FeaturedCategories/FeaturedCategories.jsx
+++ src/pages/Home/FeaturedCategories/FeaturedCategories.jsx
@@ -20,69 +20,69 @@ const FeaturedCategories = () => {
     return (
         <div className=''>
             <h2 className='text-2xl font-semibold text-center pb-12 lg:mt-0 md:mt-0 mt-8'>Featured Categories</h2>
-            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-8 justify-items-center font-semibold text-md'>
+            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-8 justify-items-center font-semibold text-md '>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc1} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc1} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-md text-[#454457] text-center'>Phones</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc2} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc2} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Power Bank</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc3} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc3} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#605e8f] text-center'>Speakers</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc4} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc4} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Camera/Gimbal</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc5} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc5} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Cases & Screen <br /> Protector</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc6} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc6} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Cable & Adapter</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc7} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc7} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>iPad</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc8} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc8} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-md text-[#454457] text-center'>Headset</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc9} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc9} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-md text-[#454457] text-center'>Car Accessories</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc10} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc10} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Wearables</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc11} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc11} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Mac</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc12} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
-                    <p className='text-md font-bold text-[#454457] text-center'>Video Games</p>
+                    <img src={fc12} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
+                    <p className='text-[#454457] text-center'>Video Games</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc13} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc13} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Earbuds</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc14} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc14} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>AirPods</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc15} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc15} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>Tablets</p>
                 </div>
                 <div className='grid grid-cols-1 gap-4'>
-                    <img src={fc16} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50' />
+                    <img src={fc16} alt="" className='w-28 h-28 border border-orange-600 rounded-full p-1 border-opacity-50 bg-white shadow-lg' />
                     <p className='text-[#454457] text-center'>All Categories</p>
                 </div>
             </div>
