125c75ef0e8fddd4c7df6d41a8221b02519a0fe4
Md. Sakib Mia <sakibatreus@gmail.com>
2025-05-12 23:06:53 +0600

    auth provider added

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
-
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
